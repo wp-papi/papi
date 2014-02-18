c6e29dfe3e8fbb6546df9a96633d821692148a04
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2014-02-18 14:17:25 +0100

    Started to rewrite the properties to classes

---
+++ includes/properties/class-property-string.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Page Type Builder - Property String
+ */
+
+class PropertyString extends PTB_Property {
+
+  /**
+   * Get the html for output.
+   *
+   * @since 1.0
+   *
+   * @return string
+   */
+
+  public function html () {}
+
+}
