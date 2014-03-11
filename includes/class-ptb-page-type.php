548122fa511345617b5cff76ebb4e88e1a5e8388
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2014-03-11 08:26:37 +0100

    Added ptb exception class and started moving code from ptb functions to page type class

--- includes/class-ptb-page-type.php
+++ includes/class-ptb-page-type.php
@@ -47,15 +47,55 @@ class PTB_Page_Type {
    */
   
   public function __construct ($file) {
-    if (file_exists($file)) {
-      $class_name = get_ptb_class_name($file);
-      $page_type = 'page_type';
-      $file_name = ptb_remove_ptb(basename($file, '.php'));
-      $fields = (object)$class_name::$page_type;
-      foreach ($fields as $key => $value) {
-        $this->$key = $value;
+    if (!file_exists($file)) {
+      throw new PTB_Exception();
+    }
+    
+    $class_name = get_ptb_class_name($file);
+    $page_type = 'page_type';
+    $file_name = ptb_remove_ptb(basename($file, '.php'));
+    $fields = (object)$class_name::$page_type;
+    foreach ($fields as $key => $value) {
+      $this->$key = $value;
+    }
+  }
+  
+  private function load_file ($file) {
+    if (!file_exists($file)) {
+      return null;
+    }
+    
+    $this->class_name = $this->get_class_name($file);
+    $this->file_name = ptb_remove_ptb(basename($file, '.php'));
+  }
+
+  /**
+   * Get class name from page type file.
+   *
+   * @param string $file
+   * @since 1.0
+   *
+   * @return string|null
+   */
+  
+  private function get_class_name ($file) {
+    $content = file_get_contents($file);
+    $tokens = token_get_all($content);
+    $class_token = false;
+    $class_name = null;
+
+    foreach ($tokens as $token) {
+      if (is_array($token)) {
+        if ($token[0] === T_CLASS) {
+          $class_token = true;
+        } else if ($class_token && $token[0] === T_STRING) {
+          $class_name = $token[1];
+          $class_token = false;
+        }
       }
     }
+
+    return $class_name;
   }
 
 }
