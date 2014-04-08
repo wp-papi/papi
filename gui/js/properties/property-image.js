d89784da31abc707cb4d6b3160219a114d4e8832
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2014-04-08 22:33:52 +0200

    Moved properties css to scss

--- gui/js/properties/property-image.js
+++ gui/js/properties/property-image.js
@@ -1,15 +1,14 @@
 (function ($) {
-  
+
   // Use WordPress media uploader to set images on property image.
   if ($('[data-ptb-property="image"]').length) {
     $('body').on('click', '[data-ptb-property="image"]', function (e) {
       e.preventDefault();
-      
-      var $this = $(this)
-        , $target = $this.prev();
+
+      var $this = $(this);
 
 			// Todo: when removing image, remove style attribute
-      
+
       Ptb.Utils.wp_media_editor($this, function (attachment) {
         if (Ptb.Utils.is_image(attachment.url)) {
 					$this.attr('style', 'height:auto');
@@ -19,5 +18,5 @@
       });
     });
   }
-  
+
 }(window.jQuery));
--- gui/js/properties/property-url.js
+++ gui/js/properties/property-url.js
@@ -1,15 +1,15 @@
 (function ($) {
-  
+
   // Use WordPress media uploader to set files on property url.
   if ($('[data-ptb-action="mediauploader"]').length) {
     $('body').on('click', '[data-ptb-action="mediauploader"]', function (e) {
       e.preventDefault();
-      
+
       var $this = $(this)
         , $target = $this.prev();
-      
-      ptb.wp_media_editor($this, $target);
+
+      Ptb.Utils.wp_media_editor($this, $target);
     });
   }
-  
+
 }(window.jQuery));
