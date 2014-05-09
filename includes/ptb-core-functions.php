1b0e26df01112c70c1d9431ee976756e0bbae454
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2014-05-09 11:33:21 +0200

    Added functions files

---
+++ includes/ptb-core-functions.php
@@ -0,0 +1,13 @@
+<?php
+
+/** 
+ * Page Type Builder Core Functions.
+ *
+ * @category Core
+ * @version 1.0.0
+ */
+ 
+// Exit if accessed directly
+if (!defined('ABSPATH')) exit;
+
+// TODO - Move all core stuff here.
