6883cd15fcd3598711daaa4eb476f2c013287dbb
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2014-09-24 21:57:43 +0200

    Saving date as unix timestamp. For now the present format in WordPress is Y-m-d

--- includes/properties/class-property-date.php
+++ includes/properties/class-property-date.php
@@ -24,10 +24,39 @@ class PropertyDate extends Papi_Property {
 
     // Database value.
     $value = $this->get_value();
+    $value = date('Y-m-d', $value);
 
     ?>
     <input type="text" name="<?php echo $options->slug; ?>" value="<?php echo $value; ?>" class="<?php echo $this->css_classes(); ?>" data-papi-property="date" />
     <?php
+  }
+
+  /**
+   * Convert value to integer.
+   *
+   * @param string $value
+   * @param int $post_id
+   * @since 1.0.0
+   *
+   * @return int
+   */
+
+  public function format_value ($value, $post_id) {
+    return intval($value);
+  }
+
+  /**
+   * Save the date as Unix timestamp.
+   *
+   * @param string $value
+   * @param int $post_id
+   * @since 1.0.0
+   *
+   * @return int
+   */
+
+  public function update_value ($value, $post_id) {
+    return strtotime($value);
   }
 
 }
