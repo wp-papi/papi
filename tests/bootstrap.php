8cf2f7135ffa1643f975d156f88e4f2266a71bf3
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2014-11-15 23:15:44 +0100

    Added back test root so Travis works

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -16,6 +16,10 @@ if ( getenv( 'WP_DEVELOP_DIR' ) !== false ) {
 	$test_root = getenv( 'WP_DEVELOP_DIR' );
 } else if ( file_exists( '../../../../tests/phpunit/includes/bootstrap.php' ) ) {
 	$test_root = '../../../../';
+} else if ( file_exists( '/tmp/wordpress-tests-lib/includes/bootstrap.php' ) ) {
+	$test_root = '/tmp/wordpress-tests-lib/';
+} else if ( file_exists( '../../../develop/tests/phpunit/includes/bootstrap.php' ) ) {
+	$test_root = '../../../develop/tests/phpunit/';
 }
 
 require $test_root . '/includes/functions.php';
