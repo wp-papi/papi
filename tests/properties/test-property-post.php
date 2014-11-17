5611a7b0c45a0af2350403582a247d5edb7cab71
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2014-11-17 23:54:34 +0100

    Added basic test for property post and deleted old test for property. Output html in phpunit isnt nice

---
+++ tests/properties/test-property-post.php
@@ -0,0 +1,31 @@
+<?php
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Unit tests covering page type functionality.
+ *
+ * @package Papi
+ */
+class WP_Property_Post extends WP_UnitTestCase {
+
+	/**
+	 * Test property post options.
+	 *
+	 * @since 1.0.0
+	 */
+
+	public function test_property_post_options () {
+		$property = _papi_get_property_options( array(
+			'type'  => 'post',
+			'title' => 'Post',
+			'slug'  => 'post'
+		) );
+
+		$this->assertEquals($property->slug, 'papi_post');
+	}
+
+}
