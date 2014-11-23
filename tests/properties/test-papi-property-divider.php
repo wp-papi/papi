2c2b5776ba0acf22caf210af1d785db7151f743d
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2014-11-23 15:18:49 +0100

    Property divider test

---
+++ tests/properties/test-papi-property-divider.php
@@ -0,0 +1,44 @@
+<?php
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Unit tests covering property functionality.
+ *
+ * @package Papi
+ */
+
+class WP_Papi_Property_Divider extends WP_UnitTestCase {
+
+	/**
+	 * Setup the test and register the page types directory.
+	 *
+	 * @since 1.0.0
+	 */
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->post_id = $this->factory->post->create();
+
+		$this->property = papi_property( array(
+			'type'  => 'divider',
+			'title' => 'Divider'
+		) );
+	}
+
+	/**
+	 * Test property options.
+	 *
+	 * @since 1.0.0
+	 */
+
+	public function test_property_options() {
+		$this->assertEquals( 'divider', $this->property->type );
+		$this->assertEquals( 'Divider', $this->property->title );
+	}
+
+}
