3e3bd5b385f8a51fa48908f5b9e436d725ea164d
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2014-11-24 09:19:06 +0100

    Fixed so datetime property works in repeater

--- gui/js/properties/datetime.js
+++ gui/js/properties/datetime.js
@@ -9,23 +9,44 @@
      */
 
     init: function () {
+      this.pikaday($('.inside > .papi-table > tbody > tr > td > input.papi-property-datetime'));
+    },
+
+    pikaday: function ($prop) {
       // Don't proceed if Pikaday is undefined.
       if (window.Pikaday === undefined) {
         return;
       }
 
-      var $prop    = $('input.papi-property-datetime');
-
       if (!$prop.length) {
         return;
       }
 
-      var settings = $prop.data();
+      var settings = $prop.data().settings;
 
       settings['field'] = $prop[0];
 
       new Pikaday(settings);
+    },
+
+    /**
+     * Initialize pikaday field when added to repeater.
+     *
+     * @param object $this
+     */
+
+     updateSelect: function ($this) {
+      this.pikaday($this.prev());
     }
   };
+
+  // Events
+
+  $(document).on('papi/property/repeater/added', '[value="datetime"]', function (e) {
+    e.preventDefault();
+
+    papi.properties.datetime.updateSelect($(this));
+  });
+
 
 })(jQuery);
