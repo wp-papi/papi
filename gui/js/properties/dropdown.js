1699d74df397bc60d9f6e64f6c9f578a47745a55
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2014-11-25 09:28:36 +0100

    Fixed hasClass check, dots should not be there

--- gui/js/properties/dropdown.js
+++ gui/js/properties/dropdown.js
@@ -7,7 +7,7 @@
     updateSelect: function ($this) {
       var $select = $this.parent().find('select')
 
-      if ($select.hasClass('.papi-vendor-select2')) {
+      if ($select.hasClass('papi-vendor-select2')) {
         $select.select2();
       }
     }
