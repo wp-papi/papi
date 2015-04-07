0d9d1228119a660bf7da1dbf9cd644b4e442291a
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-04-07 21:40:18 +0200

    Added more post test to cover all cases

--- tests/includes/lib/post-test.php
+++ tests/includes/lib/post-test.php
@@ -26,6 +26,19 @@ class Papi_Lib_Post_Test extends WP_UnitTestCase {
 		$this->assertEquals( 1, papi_get_post_id( 1 ) );
 		$this->assertEquals( $post_id, papi_get_post_id() );
 		$this->assertEquals( $post_id, papi_get_post_id( null ) );
+
+		$this->assertEquals( $post_id, papi_get_post_id( $post ) );
+		$this->assertEquals( 1, papi_get_post_id( '1' ) );
+
+		$post = null;
+
+		$_GET = array( 'post' => $post_id );
+		$this->assertEquals( $post_id, papi_get_post_id() );
+		unset( $_GET );
+
+		$_GET = array( 'page_id' => $post_id );
+		$this->assertEquals( $post_id, papi_get_post_id() );
+		unset( $_GET );
 	}
 
 	/**
@@ -35,21 +48,45 @@ class Papi_Lib_Post_Test extends WP_UnitTestCase {
 	 */
 
 	public function test_papi_get_wp_post_type() {
-		global $post, $_GET, $_POST;
+		global $post;
 
-		$post = null;
 		$this->assertEmpty( papi_get_wp_post_type() );
+
 		$_GET = array( 'post_type' => 'post' );
 		$this->assertEquals( 'post', papi_get_wp_post_type() );
 
 		$_GET = array( 'page' => 'papi-add-new-page,books' );
 		$this->assertEquals( 'books', papi_get_wp_post_type() );
 
+		$_GET = array( 'page' => 'papi-add-new-page,dash-post' );
+		$this->assertEquals( 'dash-post', papi_get_wp_post_type() );
+
+		$_GET = array( 'page' => 'papi-add-new-page,und_post' );
+		$this->assertEquals( 'und_post', papi_get_wp_post_type() );
+
+		$_GET = array( 'page' => 'papi-add-new-page,3414' );
+		$this->assertEquals( '3414', papi_get_wp_post_type() );
+
+		$_GET = array( 'page' => 'papi-add-new-page,dash13' );
+		$this->assertEquals( 'dash13', papi_get_wp_post_type() );
+
+		$_GET = array( 'page' => '' );
+		$this->assertEmpty( papi_get_wp_post_type() );
+
+		$_GET = array( 'page' => 'papi-add-new-page,' );
+		$this->assertEmpty( papi_get_wp_post_type() );
+		unset( $_GET );
+
 		$_POST = array( 'post_type' => 'page' );
 		$this->assertEquals( 'page', papi_get_wp_post_type() );
+		unset( $_POST );
 
-		$_GET  = array();
-		$_POST = array();
+		$_SERVER['REQUEST_URI'] = 'wordpress/wp-admin/post-new.php';
+		$this->assertEquals( 'post', papi_get_wp_post_type() );
+
+		$post_id = $this->factory->post->create();
+		$post = get_post( $post_id );
+		$this->assertEquals( 'post', papi_get_wp_post_type() );
 	}
 
 }
