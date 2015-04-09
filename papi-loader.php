a0057c44662cd123c62323897aa193d4441b2872
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-04-09 11:49:30 +0200

    Updated version number

--- papi-loader.php
+++ papi-loader.php
@@ -5,7 +5,7 @@
  * Description: Page Type API
  * Author: Fredrik Forsmo
  * Author URI: http://forsmo.me
- * Version: 1.2.0
+ * Version: 1.2.5
  * Plugin URI: https://wp-papi.github.io
  * Textdomain: papi
  */
