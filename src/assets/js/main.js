cc4b37c8bf29d5083d727d47c560d04f962af952
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-04-29 07:47:23 +0200

    Removed all unnecessary code from main file

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,13 +1,2 @@
-import $ from 'jquery';
 import * as Papi from 'papi/main';
-import * as Properties from 'papi-properties/main';
-
-// Extend Papi object.
-$.extend(window.papi || {}, {
-  properties: {},
-  views: {}
-});
-
-// Initialize all packages.
 Papi.init();
-Properties.init();
