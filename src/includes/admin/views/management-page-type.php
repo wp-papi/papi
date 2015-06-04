50f0480d7ddff69ccccf460802b8e0e92e41edaa
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-06-04 23:44:51 +0200

    Fixed so management page type can render tabs again

--- src/includes/admin/views/management-page-type.php
+++ src/includes/admin/views/management-page-type.php
@@ -1,3 +1,62 @@
+<?php
+
+if ( ! function_exists( 'papi_management_page_type_render_box' ) ) {
+	function papi_management_page_type_render_box( $properties, $tab = false ) {
+		?>
+		<table class="papi-table">
+			<thead>
+				<tr>
+					<?php if ( $tab ): ?>
+						<th><?php _e( 'Tab title', 'papi' ); ?></th>
+						<th><?php _e( 'Sort order', 'papi' ); ?></th>
+						<th><?php _e( 'Properties', 'papi' ); ?></th>
+					<?php else: ?>
+						<th><?php _e( 'Title', 'papi' ); ?></th>
+						<th><?php _e( 'Type', 'papi' ); ?></th>
+						<th><?php _e( 'Slug', 'papi' ); ?></th>
+						<th><?php _e( 'Sort order', 'papi' ); ?></th>
+					<?php endif; ?>
+				</tr>
+			</thead>
+			<tbody>
+				<?php
+				$properties = papi_populate_properties( $properties );
+
+				if ( $tab ) {
+					$properties = papi_setup_tabs( $properties );
+				}
+
+				foreach ( $properties as $property ): ?>
+					<tr>
+						<?php if ( isset( $property->options ) && isset( $property->options->title ) ): ?>
+							<td>
+								<?php esc_html_e( $property->options->title ); ?>
+								<br />
+								<br />
+								<?php echo __( 'Properties', 'papi' ) . ': ' . strval( count( $property->properties ) ); ?>
+							</td>
+							<td>
+								<?php esc_html_e( $property->options->sort_order ); ?>
+							</td>
+							<td>
+								<?php papi_management_page_type_render_box( $property->properties ); ?>
+							</td>
+						<?php else: ?>
+							<td><?php esc_html_e( $property->title ); ?></td>
+							<td><?php esc_html_e( $property->type ); ?></td>
+							<td><?php esc_html_e( $property->get_slug( true ) ); ?></td>
+							<td><?php esc_html_e( $property->sort_order ); ?></td>
+						<?php endif; ?>
+					</tr>
+				<?php endforeach; ?>
+				</tr>
+			</tbody>
+		</table>
+		<?php
+	}
+}
+
+?>
 <div class="wrap">
 	<div class="papi-options-logo"></div>
 	<h2><?php echo papi()->name; ?></h2>
@@ -56,55 +115,7 @@
 					<span class="papi-pull-right"><?php esc_html_e( $top_right_text . ': ' . strval( $counter ) ); ?></span>
 				</h3>
 				<div class="inside">
-					<table class="papi-table">
-						<thead>
-							<tr>
-								<?php if ( $tab ): ?>
-									<th><?php _e( 'Tab title', 'papi' ); ?></th>
-									<th><?php _e( 'Sort order', 'papi' ); ?></th>
-									<th><?php _e( 'Properties', 'papi' ); ?></th>
-								<?php else: ?>
-									<th><?php _e( 'Title', 'papi' ); ?></th>
-									<th><?php _e( 'Type', 'papi' ); ?></th>
-									<th><?php _e( 'Slug', 'papi' ); ?></th>
-									<th><?php _e( 'Sort order', 'papi' ); ?></th>
-								<?php endif; ?>
-							</tr>
-						</thead>
-						<tbody>
-							<?php
-							$properties = papi_populate_properties( $box[1] );
-
-							if ( $tab ) {
-								$properties = papi_setup_tabs( $properties );
-							}
-
-							foreach ( $properties as $property ): ?>
-								<tr>
-									<?php if ( isset( $property->options ) && isset( $property->options->title ) ): ?>
-										<td>
-											<?php esc_html_e( $property->options->title ); ?>
-											<br />
-											<br />
-											<?php echo __( 'Properties', 'papi' ) . ': ' . trval( count( $property->properties ) ); ?>
-										</td>
-										<td>
-											<?php esc_html_e( $property->options->sort_order ); ?>
-										</td>
-										<td>
-											<?php papi_management_page_type_render_box( $property->properties ); ?>
-										</td>
-									<?php else: ?>
-										<td><?php esc_html_e( $property->title ); ?></td>
-										<td><?php esc_html_e( $property->type ); ?></td>
-										<td><?php esc_html_e( papi_remove_papi( $property->slug ) ); ?></td>
-										<td><?php esc_html_e( $property->sort_order ); ?></td>
-									<?php endif; ?>
-								</tr>
-							<?php endforeach; ?>
-							</tr>
-						</tbody>
-					</table>
+					<?php papi_management_page_type_render_box( $box[1], $tab ); ?>
 				</div>
 			</div>
 
