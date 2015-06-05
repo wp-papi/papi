ef6fa3ac6a2e1bd4958daffe3e6c23a21a85e7c3
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-06-05 08:15:01 +0200

    Use strict comparison instead

--- src/includes/page-type/class-papi-page-type.php
+++ src/includes/page-type/class-papi-page-type.php
@@ -149,7 +149,7 @@ class Papi_Page_Type extends Papi_Page_Type_Meta {
 	 */
 
 	public function get_boxes() {
-		if ( empty( $this->boxes ) && $this->load_boxes == false ) {
+		if ( empty( $this->boxes ) && $this->load_boxes === false ) {
 			if ( ! method_exists( $this, 'register' ) ) {
 				return;
 			}
