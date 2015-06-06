f99ffafdddc294ad21515ed947cfaf3550ba876d
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-06-06 16:00:30 +0200

    Use plain arrays instead of papi property objects

--- tests/fixtures/page-types/properties-page-type.php
+++ tests/fixtures/page-types/properties-page-type.php
@@ -297,25 +297,25 @@ class Properties_Page_Type extends Papi_Page_Type {
 						'list' => [
 							'title' => 'List',
 							'items' => [
-								papi_property( [
+								[
 									'type'     => 'repeater',
 									'title'    => 'Repeater test',
 									'slug'     => 'repeater_test_other',
 									'settings' => [
 										'items' => [
-											papi_property( [
+											[
 												'type'  => 'string',
 												'title' => 'Book name',
 												'slug'  => 'book_name'
-											] ),
-											papi_property( [
+											],
+											[
 												'type'  => 'bool',
 												'title' => 'Is open?',
 												'slug'  => 'is_open'
-											] )
+											]
 										]
 									]
-								] )
+								]
 							]
 						]
 					]
