ab065c9f1b5693f34dd4f931df89309ffa34c0fd
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-06-09 23:05:58 +0200

    Nicer property divider

--- src/includes/properties/class-papi-property-divider.php
+++ src/includes/properties/class-papi-property-divider.php
@@ -18,28 +18,34 @@ class Papi_Property_Divider extends Papi_Property {
 	public function html() {
 		$options = $this->get_options();
 		?>
-		<h3 class="hndle papi-property-divider">
-			<span><?php echo $options->title; ?></span>
-		</h3>
+		<div class="papi-property-divider">
+			<h3>
+				<span><?php echo $options->title; ?></span>
+			</h3>
 
-		<?php if ( ! papi_is_empty( $options->description ) ): ?>
-			<p><?php echo $options->description; ?></p>
-		<?php endif; ?>
+			<?php if ( ! papi_is_empty( $options->description ) ): ?>
+				<p><?php echo $options->description; ?></p>
+			<?php endif; ?>
+		</div>
 	<?php
 	}
 
 	/**
-	 * Render the final html that is displayed in a table.
+	 * Render the final html that is displayed in the table.
 	 */
 
-	public function render() {
-		?>
-		<tr class="papi-fullwidth">
-			<td colspan="2">
-				<?php $this->html(); ?>
-			</td>
-		</tr>
-	<?php
+	public function render_row_html() {
+		if ( ! $this->get_option( 'raw' ) ):
+			?>
+			<tr class="<?php echo $this->display ? '' : 'papi-hide'; ?>">
+				<td colspan="2">
+					<?php $this->html(); ?>
+				</td>
+			</tr>
+		<?php
+		else:
+			parent::render_row_html();
+		endif;
 	}
 
 }
