c4a50096928940111a593da7104695fa6a6506c8
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-06-22 15:22:05 +0200

    Hidden editor on option page + output right page title

--- src/includes/option-type/class-papi-option-type.php
+++ src/includes/option-type/class-papi-option-type.php
@@ -63,9 +63,12 @@ class Papi_Option_Type extends Papi_Page_Type {
 	public function render() {
 		?>
 		<div class="wrap">
-			<h2>Header</h2>
+			<h2><?php esc_html_e( $this->name ); ?></h2>
 			<form id="post" method="post" name="post">
-				<?php wp_nonce_field( 'papi_save_data', 'papi_meta_nonce' ); ?>
+				<div id="papi-hidden-editor" class="hide-if-js">
+					<?php wp_nonce_field( 'papi_save_data', 'papi_meta_nonce' ); ?>
+					<?php wp_editor( '', 'papiHiddenEditor' ); ?>
+				</div>
 				<div id="poststuff">
 					<div id="post-body">
 						<?php do_meta_boxes( $this->post_type[0], 'normal', null ); ?>
