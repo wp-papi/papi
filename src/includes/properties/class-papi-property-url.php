b791e06a60f73d2c1037231692ae9f92ecb2aa9b
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-07-09 09:34:42 +0200

    Revert "Fixed so urls with åäö works to validate"
    This reverts commit af5a24c3032ecfc01098bcdd2f986c8ce545eb51.

--- src/includes/properties/class-papi-property-url.php
+++ src/includes/properties/class-papi-property-url.php
@@ -51,7 +51,9 @@ class Papi_Property_Url extends Papi_Property {
 	 */
 
 	public function load_value( $value, $slug, $post_id ) {
-		$this->update_value( $value, $slug, $post_id );
+		if ( filter_var( $value, FILTER_VALIDATE_URL ) ) {
+			return $value;
+		}
 	}
 
 	/**
@@ -65,17 +67,7 @@ class Papi_Property_Url extends Papi_Property {
 	 */
 
 	public function update_value( $value, $slug, $post_id ) {
-		$test_value = $value;
-
-		if ( $parts = parse_url( $value ) ) {
-			if ( isset( $parts['path'] ) && $ext = pathinfo( $parts['path'], PATHINFO_EXTENSION ) ) {
-				$file = basename( $parts['path'] );
-				$file = str_replace( '.' . $ext, '', $file );
-				$test_value = str_replace( $file, papi_slugify( $file ), $value );
-			}
-		}
-
-		if ( filter_var( $test_value, FILTER_VALIDATE_URL ) ) {
+		if ( filter_var( $value, FILTER_VALIDATE_URL ) ) {
 			return $value;
 		}
 	}
