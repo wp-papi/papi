4c600541de34c17894290efa39101fb704966a2f
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-07-21 13:27:12 +0200

    Fixed page type columns test

--- tests/includes/admin/class-papi-admin-test.php
+++ tests/includes/admin/class-papi-admin-test.php
@@ -47,6 +47,10 @@ class Papi_Admin_Test extends WP_UnitTestCase {
 
 	public function test_manage_page_type_posts_columns() {
 		$arr = $this->admin->manage_page_type_posts_columns( [] );
+		$this->assertEmpty( $arr );
+
+		$_GET['post_type'] = 'page';
+		$arr = $this->admin->manage_page_type_posts_columns( [] );
 		$this->assertEquals( ['page_type' => 'Page Type'], $arr );
 	}
 
