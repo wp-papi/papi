f024f8502abe3b5295cf787b6ea78ce82941a4f1
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-07-27 15:04:01 +0200

    Removed comments

--- tests/papi-loader-test.php
+++ tests/papi-loader-test.php
@@ -11,18 +11,9 @@ defined( 'ABSPATH' ) || exit;
 
 class Papi_Loader_Test extends WP_UnitTestCase {
 
-	/**
-	 * Test so Papi plugin is loaded correct.
-	 */
-
 	public function test_plugin_activated() {
 		$this->assertTrue( class_exists( 'Papi_Loader' ) && class_exists( 'Papi_Admin' ) );
 	}
-
-	/**
-	 * The action `plugins_loaded` should have the `papi` hook
-	 * and should have a default priority of 10.
-	 */
 
 	public function test_after_setup_theme_action() {
 		$this->assertEquals( 10, has_action( 'plugins_loaded', 'papi' ) );
