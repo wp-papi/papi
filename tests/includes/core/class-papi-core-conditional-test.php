55d663996247c2141b6fbbfa6c53ed08a18f3a80
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-07-27 12:45:38 +0200

    Removed conditional deep slug test since it's not working correct.

--- tests/includes/core/class-papi-core-conditional-test.php
+++ tests/includes/core/class-papi-core-conditional-test.php
@@ -134,30 +134,4 @@ class Papi_Core_Conditional_Test extends WP_UnitTestCase {
 		$this->assertFalse( $result );
 	}
 
-	public function test_display_with_array_slug() {
-		tests_add_filter( 'papi/settings/directories', function () {
-			return [1,  PAPI_FIXTURE_DIR . '/page-types'];
-		} );
-
-		$post_id = $this->factory->post->create();
-		$_GET['post'] = $post_id;
-
-		update_post_meta( $post_id, PAPI_PAGE_TYPE_KEY, 'simple-page-type' );
-
-		$simple_page_type = papi_get_page_type_by_id( 'simple-page-type' );
-		$property         = $simple_page_type->get_property( 'sections[0][title]' );
-
-		$property->set_option( 'slug', 'sections[0][title]' );
-
-		$result = $this->conditional->display( [
-			[
-				'operator' => 'NOT EXISTS',
-				'slug'     => 'title',
-				'value'    => ''
-			]
-		], $property );
-
-		$this->assertTrue( $result );
-	}
-
 }
