5ead17431e2b110c2d3b365c1bb79ad7c58a7f93
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-07-30 07:55:36 +0200

    Fixed failing save meta box test

--- tests/includes/admin/class-papi-admin-post-handler-test.php
+++ tests/includes/admin/class-papi-admin-post-handler-test.php
@@ -138,18 +138,16 @@ class Papi_Admin_Post_Handler_Test extends WP_UnitTestCase {
 			'value' => 'Hello, world!'
 		], $_POST );
 
-		$user_id = $this->factory->user->create( [ 'role' => 'administrator' ] );
+		$user_id = $this->factory->user->create( [ 'role' => 'read' ] );
 		wp_set_current_user( $user_id );
 
 		$_POST['papi_meta_nonce'] = wp_create_nonce( 'papi_save_data' );
 		$_POST['post_ID'] = $this->post_id;
 
-		define( 'DOING_AUTOSAVE', true );
-
 		$this->handler->save_meta_boxes( $this->post_id, get_post( $this->post_id ) );
 		wp_set_current_user( 0 );
 
-		// doing autosave is true
+		// wrong capability
 		$this->assertNull( papi_get_field( $this->post_id, $property->slug ) );
 	}
 
@@ -162,16 +160,18 @@ class Papi_Admin_Post_Handler_Test extends WP_UnitTestCase {
 			'value' => 'Hello, world!'
 		], $_POST );
 
-		$user_id = $this->factory->user->create( [ 'role' => 'read' ] );
+		$user_id = $this->factory->user->create( [ 'role' => 'administrator' ] );
 		wp_set_current_user( $user_id );
 
 		$_POST['papi_meta_nonce'] = wp_create_nonce( 'papi_save_data' );
 		$_POST['post_ID'] = $this->post_id;
 
+		define( 'DOING_AUTOSAVE', true );
+
 		$this->handler->save_meta_boxes( $this->post_id, get_post( $this->post_id ) );
 		wp_set_current_user( 0 );
 
-		// wrong capability
+		// doing autosave is true
 		$this->assertNull( papi_get_field( $this->post_id, $property->slug ) );
 	}
 
