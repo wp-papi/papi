ba1e65d5d30df9f96c32f3a2237f20ab62bbdf01
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-08-06 20:56:11 +0200

    Added import_value method to bool property

--- src/includes/properties/class-papi-property-bool.php
+++ src/includes/properties/class-papi-property-bool.php
@@ -75,6 +75,19 @@ class Papi_Property_Bool extends Papi_Property {
 	}
 
 	/**
+	 * Import value to the property.
+	 *
+	 * @param mixed $value
+	 * @param string $slug
+	 * @param int $post_id
+	 *
+	 * @return mixed
+	 */
+	public function import_value( $value, $slug, $post_id ) {
+		return $this->format_value( $value, $slug, $post_id );
+	}
+
+	/**
 	 * Fix the database value on update.
 	 *
 	 * @param mixed $value
