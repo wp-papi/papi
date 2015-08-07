25b1fab6bef4ded9d1be614473b4a01400b23af3
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-08-07 17:06:25 +0200

    Fix code style

--- src/includes/porter/class-papi-porter-driver-core.php
+++ src/includes/porter/class-papi-porter-driver-core.php
@@ -7,74 +7,74 @@
  */
 class Papi_Porter_Driver_Core extends Papi_Porter_Driver {
 
-    /**
-     * The driver name.
-     *
-     * @var string
-     */
-    protected $name = 'core';
+	/**
+	 * The driver name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'core';
 
-    /**
-     * Alias for driver name.
-     *
-     * @var array|string
-     */
-    protected $alias = 'papi';
+	/**
+	 * Alias for driver name.
+	 *
+	 * @var array|string
+	 */
+	protected $alias = 'papi';
 
-    /**
-     * Get value that should be saved.
-     *
-     * @param array $options
-     *
-     * @return mixed
-     */
-    public function get_value( array $options = [] ) {
-        if ( ! isset( $options['post_id'] ) || ! is_int( $options['post_id'] ) ) {
-            throw new InvalidArgumentException( 'Missing `post_id` option. Should be int.' );
-        }
+	/**
+	 * Get value that should be saved.
+	 *
+	 * @param array $options
+	 *
+	 * @return mixed
+	 */
+	public function get_value( array $options = [] ) {
+		if ( ! isset( $options['post_id'] ) || ! is_int( $options['post_id'] ) ) {
+			throw new InvalidArgumentException( 'Missing `post_id` option. Should be int.' );
+		}
 
-        if ( ! isset( $options['property'] ) || $options['property'] instanceof Papi_Core_Property === false ) {
-            throw new InvalidArgumentException( 'Missing `property` option. Should be instance of `Papi_Core_Property`.' );
-        }
+		if ( ! isset( $options['property'] ) || $options['property'] instanceof Papi_Core_Property === false ) {
+			throw new InvalidArgumentException( 'Missing `property` option. Should be instance of `Papi_Core_Property`.' );
+		}
 
-        if ( ! isset( $options['slug'] ) || empty( $options['slug'] ) ) {
-            $options['slug'] = $options['property']->get_slug( true );
-        }
+		if ( ! isset( $options['slug'] ) || empty( $options['slug'] ) ) {
+			$options['slug'] = $options['property']->get_slug( true );
+		}
 
-        if ( ! isset( $options['value'] ) ) {
-            throw new InvalidArgumentException( 'Missing `value` option.' );
-        }
+		if ( ! isset( $options['value'] ) ) {
+			throw new InvalidArgumentException( 'Missing `value` option.' );
+		}
 
-        $value = $this->call_value( $options['value'] );
-        $value = $this->update_value( $options['property'], $value, $options['slug'], $options['post_id'] );
+		$value = $this->call_value( $options['value'] );
+		$value = $this->update_value( $options['property'], $value, $options['slug'], $options['post_id'] );
 
-        return maybe_unserialize( $value );
-    }
+		return maybe_unserialize( $value );
+	}
 
-    /**
-     * Update value.
-     *
-     * @param Papi_Core_Property $property
-     * @param mixed $value
-     * @param string $slug
-     * @param int $post_id
-     *
-     * @return array
-     */
-    protected function update_value( $property, $value, $slug, $post_id ) {
-        if ( ! is_array( $value ) || ! $this->should_update_array( $slug ) ) {
-            return $property->import_value( $value, $slug, $post_id );
-        }
+	/**
+	 * Update value.
+	 *
+	 * @param Papi_Core_Property $property
+	 * @param mixed $value
+	 * @param string $slug
+	 * @param int $post_id
+	 *
+	 * @return array
+	 */
+	protected function update_value( $property, $value, $slug, $post_id ) {
+		if ( ! is_array( $value ) || ! $this->should_update_array( $slug ) ) {
+			return $property->import_value( $value, $slug, $post_id );
+		}
 
-        $old   = papi_get_field( $post_id, $slug, [] );
-        $value = array_merge( $old, $value );
-        $value = $property->import_value( $value, $slug, $post_id );
+		$old   = papi_get_field( $post_id, $slug, [] );
+		$value = array_merge( $old, $value );
+		$value = $property->import_value( $value, $slug, $post_id );
 
-        if ( $property->import_setting( 'property_array_slugs' ) ) {
-            return papi_from_property_array_slugs( $value, $slug );
-        }
+		if ( $property->import_setting( 'property_array_slugs' ) ) {
+			return papi_from_property_array_slugs( $value, $slug );
+		}
 
-        return $property->update_value( $value, $slug, $post_id );
-    }
+		return $property->update_value( $value, $slug, $post_id );
+	}
 
 }
--- src/includes/porter/class-papi-porter-driver.php
+++ src/includes/porter/class-papi-porter-driver.php
@@ -7,197 +7,197 @@
  */
 abstract class Papi_Porter_Driver {
 
-    /**
-     * The driver name.
-     *
-     * @var string
-     */
-     protected $name = '';
+	/**
+	 * The driver name.
+	 *
+	 * @var string
+	 */
+	 protected $name = '';
 
-     /**
-      * Alias for driver name.
-      *
-      * @var array|string
-      */
-     protected $alias = [];
+	 /**
+	  * Alias for driver name.
+	  *
+	  * @var array|string
+	  */
+	 protected $alias = [];
 
-    /**
-     * Options per property.
-     *
-     * @var array
-     */
-    protected $options = [
-        'custom'   => [],
-        'post_id'  => 0,
-        'property' => null,
-        'slug'     => '',
-        'value'    => null
-    ];
+	/**
+	 * Options per property.
+	 *
+	 * @var array
+	 */
+	protected $options = [
+		'custom'   => [],
+		'post_id'  => 0,
+		'property' => null,
+		'slug'     => '',
+		'value'    => null
+	];
 
-    /**
-     * Papi Porter instance.
-     *
-     * @var Papi_Porter
-     */
-    protected $porter;
+	/**
+	 * Papi Porter instance.
+	 *
+	 * @var Papi_Porter
+	 */
+	protected $porter;
 
-    /**
-     * The post id.
-     *
-     * @var int
-     */
-    protected $post_id;
+	/**
+	 * The post id.
+	 *
+	 * @var int
+	 */
+	protected $post_id;
 
-    /**
-     * The constructor.
-     *
-     * @codeCoverageIgnore
-     */
-    public function __construct() {
-    }
+	/**
+	 * The constructor.
+	 *
+	 * @codeCoverageIgnore
+	 */
+	public function __construct() {
+	}
 
-    /**
-     * Call closure value if any.
-     *
-     * @param mixed $value
-     *
-     * @return mixed
-     */
-    protected function call_value( $value ) {
-        if ( $value instanceof Closure ) {
-            return $value();
-        }
+	/**
+	 * Call closure value if any.
+	 *
+	 * @param mixed $value
+	 *
+	 * @return mixed
+	 */
+	protected function call_value( $value ) {
+		if ( $value instanceof Closure ) {
+			return $value();
+		}
 
-        return $value;
-    }
+		return $value;
+	}
 
-    /**
-     * Bootstrap the driver.
-     */
-    public function bootstrap() {
-    }
+	/**
+	 * Bootstrap the driver.
+	 */
+	public function bootstrap() {
+	}
 
-    /**
-     * Get filter key.
-     *
-     * @param string $type
-     * @param string $filter
-     *
-     * @return string
-     */
-    public function filter( $type, $filter ) {
-        if ( ! is_string( $filter ) ) {
-            $filter = '';
-        }
+	/**
+	 * Get filter key.
+	 *
+	 * @param string $type
+	 * @param string $filter
+	 *
+	 * @return string
+	 */
+	public function filter( $type, $filter ) {
+		if ( ! is_string( $filter ) ) {
+			$filter = '';
+		}
 
-        return sprintf( 'papi/porter/driver/%s/%s/%s', $this->name, $type, $filter );
-    }
+		return sprintf( 'papi/porter/driver/%s/%s/%s', $this->name, $type, $filter );
+	}
 
-    /**
-     * Get the driver name.
-     *
-     * @return string
-     */
-    public function get_driver_name() {
-        return $this->name;
-    }
+	/**
+	 * Get the driver name.
+	 *
+	 * @return string
+	 */
+	public function get_driver_name() {
+		return $this->name;
+	}
 
-    /**
-     * Get options.
-     *
-     * @return array
-     */
-    public function get_options() {
-        return $this->options;
-    }
+	/**
+	 * Get options.
+	 *
+	 * @return array
+	 */
+	public function get_options() {
+		return $this->options;
+	}
 
-    /**
-     * Get the import value for a property.
-     *
-     * @param  array  $options
-     *
-     * @return mixed
-     */
-    abstract public function get_value( array $options );
+	/**
+	 * Get the import value for a property.
+	 *
+	 * @param  array  $options
+	 *
+	 * @return mixed
+	 */
+	abstract public function get_value( array $options );
 
-    /**
-     * Set driver alias.
-     */
-    protected function set_driver_alias() {
-        foreach ( papi_to_array( $this->alias ) as $alias ) {
-            if ( is_string( $alias ) && ! $this->porter->exists( 'driver.' . $alias ) ) {
-                $this->set_driver_name( $alias );
-            }
-        }
-    }
+	/**
+	 * Set driver alias.
+	 */
+	protected function set_driver_alias() {
+		foreach ( papi_to_array( $this->alias ) as $alias ) {
+			if ( is_string( $alias ) && ! $this->porter->exists( 'driver.' . $alias ) ) {
+				$this->set_driver_name( $alias );
+			}
+		}
+	}
 
-    /**
-     * Set driver name.
-     *
-     * @param string $name
-     *
-     * @throws InvalidArgumentException if driver name is empty or not a string.
-     * @throws Exception if driver name exists.
-     */
-    protected function set_driver_name( $name ) {
-        if ( empty( $name ) || ! is_string( $name ) ) {
-            throw new InvalidArgumentException( 'Driver name is empty or not a string.' );
-        }
+	/**
+	 * Set driver name.
+	 *
+	 * @param string $name
+	 *
+	 * @throws InvalidArgumentException if driver name is empty or not a string.
+	 * @throws Exception if driver name exists.
+	 */
+	protected function set_driver_name( $name ) {
+		if ( empty( $name ) || ! is_string( $name ) ) {
+			throw new InvalidArgumentException( 'Driver name is empty or not a string.' );
+		}
 
-        $name = strtolower( $name );
+		$name = strtolower( $name );
 
-        if ( $this->porter->exists( 'driver.' . $name ) ) {
-            throw new Exception( sprintf( '`%s` driver exists.', $name ) );
-        }
+		if ( $this->porter->exists( 'driver.' . $name ) ) {
+			throw new Exception( sprintf( '`%s` driver exists.', $name ) );
+		}
 
-        $this->porter->singleton( 'driver.' . $name, get_class( $this ) );
-    }
+		$this->porter->singleton( 'driver.' . $name, get_class( $this ) );
+	}
 
-    /**
-     * Set Porter instance.
-     *
-     * @param Papi_Porter $porter
-     */
-    public function set_porter( Papi_Porter $porter ) {
-        $this->porter = $porter;
-        $this->set_driver_name( $this->name );
-        $this->set_driver_alias();
-    }
+	/**
+	 * Set Porter instance.
+	 *
+	 * @param Papi_Porter $porter
+	 */
+	public function set_porter( Papi_Porter $porter ) {
+		$this->porter = $porter;
+		$this->set_driver_name( $this->name );
+		$this->set_driver_alias();
+	}
 
-    /**
-     * Set options for properties.
-     *
-     * @param array $options
-     */
-    public function set_options( array $options = [] ) {
-        $this->options = array_merge( $this->options, $options );
-    }
+	/**
+	 * Set options for properties.
+	 *
+	 * @param array $options
+	 */
+	public function set_options( array $options = [] ) {
+		$this->options = array_merge( $this->options, $options );
+	}
 
-    /**
-     * Determine if a property should update existing array or not.
-     *
-     * @param string $slug
-     *
-     * @return bool
-     */
-    protected function should_update_array( $slug ) {
-        return is_string( $slug ) &&
-            isset( $this->options['custom'] ) &&
-            isset( $this->options['custom'][$slug] ) &&
-            $this->options['custom'][$slug]['update_array'];
-    }
+	/**
+	 * Determine if a property should update existing array or not.
+	 *
+	 * @param string $slug
+	 *
+	 * @return bool
+	 */
+	protected function should_update_array( $slug ) {
+		return is_string( $slug ) &&
+			isset( $this->options['custom'] ) &&
+			isset( $this->options['custom'][$slug] ) &&
+			$this->options['custom'][$slug]['update_array'];
+	}
 
-    /**
-     * Return the given object. Useful for chaining.
-     *
-     * @codeCoverageIgnore
-     *
-     * @param mixed $obj
-     *
-     * @return obj
-     */
-    protected function with( $obj ) {
-        return $obj;
-    }
+	/**
+	 * Return the given object. Useful for chaining.
+	 *
+	 * @codeCoverageIgnore
+	 *
+	 * @param mixed $obj
+	 *
+	 * @return obj
+	 */
+	protected function with( $obj ) {
+		return $obj;
+	}
 
 }
