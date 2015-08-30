f0db01c300296bcc3d0ffa45ea649434666a5999
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-08-30 22:01:02 +0200

    Add i18n support for pikaday #83

--- src/includes/properties/class-papi-property-datetime.php
+++ src/includes/properties/class-papi-property-datetime.php
@@ -33,9 +33,47 @@ class Papi_Property_Datetime extends Papi_Property {
 
 		$settings_json = [
 			'format'      => $settings->format,
+			'i18n'        => [
+				'previousMonth' => __( 'Previous Month', 'papi' ),
+				'nextMonth'     => __( 'Next Month', 'papi' ),
+				'midnight'      => __( 'Midnight', 'papi' ),
+				'months'        => [
+					__( 'January', 'papi' ),
+					__( 'February', 'papi' ),
+					__( 'March', 'papi' ),
+					__( 'April', 'papi' ),
+					__( 'May', 'papi' ),
+					__( 'June', 'papi' ),
+					__( 'July', 'papi' ),
+					__( 'August', 'papi' ),
+					__( 'September', 'papi' ),
+					__( 'October', 'papi' ),
+					__( 'November', 'papi' ),
+					__( 'December', 'papi' )
+				],
+				'noon'          => __( 'Noon', 'papi' ),
+				'weekdays'      => [
+					__( 'Sunday', 'papi' ),
+					__( 'Monday', 'papi' ),
+					__( 'Tuesday', 'papi' ),
+					__( 'Wednesday', 'papi' ),
+					__( 'Thursday', 'papi' ),
+					__( 'Friday', 'papi' ),
+					__( 'Saturday', 'papi' )
+				],
+				'weekdaysShort' => [
+					__( 'Sun', 'papi' ),
+					__( 'Mon', 'papi' ),
+					__( 'Tue', 'papi' ),
+					__( 'Wed', 'papi' ),
+					__( 'Thu', 'papi' ),
+					__( 'Fri', 'papi' ),
+					__( 'Sat', 'papi' )
+				]
+			],
 			'showTime'    => $settings->show_time,
 			'showSeconds' => $settings->show_seconds,
-			'use24hour'   => $settings->use_24_hours
+			'use24hour'   => $this->use_24_hours(),
 		];
 
 		papi_render_html_tag( 'input', [
@@ -46,5 +84,14 @@ class Papi_Property_Datetime extends Papi_Property {
 			'type'          => 'text',
 			'value'         => $value
 		] );
+	}
+
+	/**
+	 * Check if 24 hours should be used or not.
+	 *
+	 * @return bool
+	 */
+	protected function use_24_hours() {
+		return get_locale() === 'sv_SE' || $this->get_setting( 'use_24_hours' );
 	}
 }
