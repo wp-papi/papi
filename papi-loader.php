d69e2c871daead1ab15d4b23a60fd8dd2ed68315
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-09-03 08:01:36 +0200

    Bump version and change description text

--- papi-loader.php
+++ papi-loader.php
@@ -2,10 +2,10 @@
 
 /**
  * Plugin Name: Papi
- * Description: Page Type API
+ * Description: Page Type API with custom fields
  * Author: Fredrik Forsmo
  * Author URI: http://forsmo.me
- * Version: 2.2.1
+ * Version: 2.2.2
  * Plugin URI: https://wp-papi.github.io
  * Textdomain: papi
  */
