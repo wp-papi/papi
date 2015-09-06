929b08d8dc66fb27283aba91a90fd4c00ce9971d
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-09-06 16:29:26 +0200

    Revert "Trying to get components to merge"
    This reverts commit d8516723bf3f7adaebe7841f5624d9de69a8a161.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -18,9 +18,6 @@ import phpcs from 'gulp-phpcs';
 import phpcd from 'gulp-phpcpd';
 import plumber from 'gulp-plumber';
 import pkg from './package.json';
-import merge from 'merge-stream';
-import buffer from 'vinyl-buffer';
-import source from 'vinyl-source-stream';
 import runSequence from 'run-sequence';
 import webpack from 'webpack-stream';
 import webpackconfig from './webpack.config.js';
@@ -51,12 +48,8 @@ const config = {
     src: assets + 'scss/**/*.scss'
   },
   scripts: {
-    components: [
-      src + 'js/components/*.js'
-    ],
     entry: assets + 'js/main.js',
-    dist: dist + 'js/main.min.js',
-    lint: assets + 'js/**/*.js',
+    lint: assets + 'js/**/*.js'
   }
 };
 
@@ -137,21 +130,13 @@ gulp.task('sass', () => {
  * Webpack
  */
 gulp.task('webpack', () => {
-  const papi = gulp.src([config.scripts.entry])
+  gulp.src([config.scripts.entry])
     .pipe(plumber())
     .pipe(webpack(webpackconfig))
     .pipe(header(banner, {
       package: pkg
     }))
-    .bundle()
-    .pipe(source('main.min.js'))
-    .pipe(buffer());
-
-  const components = gulp.src(config.scripts.components);
-
-  return merge(components, papi)
-    .pipe(concat('main.min.js'))
-    .pipe(gulp.dest(config.scripts.dist));
+    .pipe(gulp.dest(`${dist}js`));
 });
 
 /**
