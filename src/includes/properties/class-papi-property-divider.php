82bb02fe6b676dae45c2f9babbcd682c15d782c7
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-09-06 12:06:31 +0200

    Remove all html from divider and use papi_html_tag

--- src/includes/properties/class-papi-property-divider.php
+++ src/includes/properties/class-papi-property-divider.php
@@ -15,33 +15,32 @@ class Papi_Property_Divider extends Papi_Property {
 	 */
 	public function html() {
 		$options = $this->get_options();
-		?>
-		<div class="papi-property-divider" data-papi-rule="<?php echo $this->html_name(); ?>">
-			<h3>
-				<span><?php echo $options->title; ?></span>
-			</h3>
 
-			<?php if ( ! papi_is_empty( $options->description ) ): ?>
-				<p><?php echo $options->description; ?></p>
-			<?php endif; ?>
-		</div>
-	<?php
+		papi_render_html_tag( 'div', [
+			'class'          => 'papi-property-divider',
+			'data-papi-rule' => $this->html_name(),
+			sprintf( '<h3><span>%s</span></h3>', $options->title )
+		] );
+
+		if ( ! papi_is_empty( $options->description ) ) {
+			echo sprintf( '<p>%s</p>', $options->description );
+		}
 	}
 
 	/**
 	 * Render the final html that is displayed in the table.
 	 */
 	public function render_row_html() {
-		if ( ! $this->get_option( 'raw' ) ):
-			?>
-			<tr class="<?php echo $this->display ? '' : 'papi-hide'; ?>">
-				<td colspan="2">
-					<?php $this->html(); ?>
-				</td>
-			</tr>
-		<?php
-		else:
+		if ( $this->get_option( 'raw' ) ) {
 			parent::render_row_html();
-		endif;
+		} else {
+			papi_render_html_tag( 'tr', [
+				'class' => $this->display ? '' : 'papi-hide',
+				papi_html_tag( 'td', [
+					'colspan' => 2,
+					$this->render_property_html()
+				] )
+			] );
+		}
 	}
 }
