a9a4df7115d547aafc4d6edbafe111210c091470
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-10-03 12:18:07 +0200

    Add format value tests for gallery property

--- tests/includes/properties/class-papi-property-gallery-test.php
+++ tests/includes/properties/class-papi-property-gallery-test.php
@@ -25,6 +25,100 @@ class Papi_Property_Gallery_Test extends Papi_Property_Test_Case {
 		$this->assertEquals( [], $this->property->default_value );
 	}
 
+	public function test_property_format_value() {
+		$post_id = $this->factory->post->create( ['post_type' => 'attachment'] );
+		$meta    = [
+			'width'      => 2900,
+			'height'     => 1559,
+			'file'       => '2011/12/press_image.jpg',
+			'sizes'      => [
+				'thumbnail' => [
+					'file'      => 'press_image-150x150.jpg',
+					'width'     => 150,
+					'height'    => 150,
+					'mime-type' => 'image/jpeg'
+				]
+			],
+			'image_meta' => [
+				'aperture'          => 5,
+				'credit'            => '',
+				'camera'            => 'Super',
+				'caption'           => '',
+				'created_timestamp'	=> 1323190643,
+				'copyright'         => '',
+				'focal_length'      => 35,
+				'iso'               => 800,
+				'shutter_speed'     => 0.016666666666667,
+				'title'             => ''
+			]
+		];
+
+		update_post_meta( $post_id, '_wp_attachment_metadata', $meta );
+		update_post_meta( $post_id, '_wp_attachment_image_alt', 'alt text' );
+		update_post_meta( $post_id, '_wp_attached_file', '2011/12/press_image.jpg' );
+		$thumbnail_url = home_url( '/wp-content/uploads/2011/12/press_image-150x150.jpg' );
+
+		tests_add_filter( 'wp_get_attachment_image_src', function ( $image, $attachment_id, $size, $icon ) use ( $thumbnail_url ) {
+			if ( $size === 'thumbnail' ) {
+				return [$thumbnail_url];
+			}
+
+			return $image;
+		}, 10, 4 );
+
+		$images = $this->property->format_value( [$post_id, $post_id], '', $post_id );
+
+		$this->assertTrue( is_object( $images[0] ) );
+		$this->assertEquals( 2900, $images[0]->width );
+		$this->assertEquals( 1559, $images[0]->height );
+		$this->assertEquals( '2011/12/press_image.jpg', $images[0]->file );
+
+		$this->assertTrue( is_array( $images[0]->sizes ) );
+		$this->assertTrue( isset( $images[0]->sizes['thumbnail'] ) );
+		$this->assertEquals( 'press_image-150x150.jpg', $images[0]->sizes['thumbnail']['file'] );
+		$this->assertEquals( 150, $images[0]->sizes['thumbnail']['width'] );
+		$this->assertEquals( 150, $images[0]->sizes['thumbnail']['height'] );
+		$this->assertEquals( 'image/jpeg', $images[0]->sizes['thumbnail']['mime-type'] );
+		$this->assertEquals( $thumbnail_url, $images[0]->sizes['thumbnail']['url'] );
+
+		$this->assertTrue( is_array( $images[0]->image_meta ) );
+		$this->assertEquals( 5, $images[0]->image_meta['aperture'] );
+		$this->assertEquals( '', $images[0]->image_meta['credit'] );
+		$this->assertEquals( 'Super', $images[0]->image_meta['camera'] );
+		$this->assertEquals( '', $images[0]->image_meta['caption'] );
+		$this->assertEquals( 1323190643, $images[0]->image_meta['created_timestamp'] );
+		$this->assertEquals( '', $images[0]->image_meta['copyright'] );
+		$this->assertEquals( 35, $images[0]->image_meta['focal_length'] );
+		$this->assertEquals( 800, $images[0]->image_meta['iso'] );
+		$this->assertEquals( 0.016666666666667, $images[0]->image_meta['shutter_speed'] );
+		$this->assertEquals( '', $images[0]->image_meta['title'] );
+
+		$this->assertTrue( is_object( $images[1] ) );
+		$this->assertEquals( 2900, $images[1]->width );
+		$this->assertEquals( 1559, $images[1]->height );
+		$this->assertEquals( '2011/12/press_image.jpg', $images[1]->file );
+
+		$this->assertTrue( is_array( $images[1]->sizes ) );
+		$this->assertTrue( isset( $images[1]->sizes['thumbnail'] ) );
+		$this->assertEquals( 'press_image-150x150.jpg', $images[1]->sizes['thumbnail']['file'] );
+		$this->assertEquals( 150, $images[1]->sizes['thumbnail']['width'] );
+		$this->assertEquals( 150, $images[1]->sizes['thumbnail']['height'] );
+		$this->assertEquals( 'image/jpeg', $images[1]->sizes['thumbnail']['mime-type'] );
+		$this->assertEquals( $thumbnail_url, $images[1]->sizes['thumbnail']['url'] );
+
+		$this->assertTrue( is_array( $images[1]->image_meta ) );
+		$this->assertEquals( 5, $images[1]->image_meta['aperture'] );
+		$this->assertEquals( '', $images[1]->image_meta['credit'] );
+		$this->assertEquals( 'Super', $images[1]->image_meta['camera'] );
+		$this->assertEquals( '', $images[1]->image_meta['caption'] );
+		$this->assertEquals( 1323190643, $images[1]->image_meta['created_timestamp'] );
+		$this->assertEquals( '', $images[1]->image_meta['copyright'] );
+		$this->assertEquals( 35, $images[1]->image_meta['focal_length'] );
+		$this->assertEquals( 800, $images[1]->image_meta['iso'] );
+		$this->assertEquals( 0.016666666666667, $images[1]->image_meta['shutter_speed'] );
+		$this->assertEquals( '', $images[1]->image_meta['title'] );
+	}
+
 	public function test_property_import_value() {
 		$this->assertEmpty( $this->property->import_value( $this->get_value(), '', 0 ) );
 	}
