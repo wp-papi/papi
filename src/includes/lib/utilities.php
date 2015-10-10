44f04a2be95f3f637006a66be2719dd398ce1524
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-10-10 19:51:58 +0200

    Comment fix

--- src/includes/lib/utilities.php
+++ src/includes/lib/utilities.php
@@ -289,7 +289,7 @@ function papi_get_or_post( $key ) {
  * Get query string if it exists and is not empty.
  *
  * @param  array|string $qs
- * @param  bool $keep_keys
+ * @param  bool         $keep_keys
  *
  * @return array|string
  */
