4a8b6b5a0803a0b161da8de8571bd1ae3dc03eb3
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-10-13 21:14:21 +0200

    Restructure tests directory

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,16 +4,16 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 // Define fixtures directory constant
-define( 'PAPI_FIXTURE_DIR', __DIR__ . '/fixtures' );
+define( 'PAPI_FIXTURE_DIR', __DIR__ . '/data' );
 
 // Load Papi loader file as plugin.
 WP_Test_Suite::load_plugins( __DIR__ . '/../papi-loader.php' );
 
 // Load our helpers file.
 WP_Test_Suite::load_files( [
-	__DIR__ . '/fixtures/functions.php',
-	__DIR__ . '/helpers.php',
-	__DIR__	. '/class-papi-property-test-case.php'
+	__DIR__ . '/data/functions.php',
+	__DIR__ . '/framework/helpers.php',
+	__DIR__	. '/framework/class-papi-property-test-case.php'
 ] );
 
 // Run the WordPress test suite.
