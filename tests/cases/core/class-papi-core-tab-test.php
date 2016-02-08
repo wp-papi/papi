cb6594bb3f005f2f60f78f4266f5772f0840a697
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-02-08 20:56:33 +0100

    Remove old test code

--- tests/cases/core/class-papi-core-tab-test.php
+++ tests/cases/core/class-papi-core-tab-test.php
@@ -12,15 +12,3 @@ class Papi_Core_Tab_Test extends WP_UnitTestCase {
 		$this->assertSame( 'Tab', $tab->title );
 		$this->assertSame( '_papi_tab', $tab->id );
 	}
-
-	public function test_excluded_options() {
-		$tab = new Papi_Core_Tab( [
-			'title'      => 'Tab',
-			'options'    => null,
-			'properties' => null,
-		] );
-		$this->assertSame( 'Tab', $tab->title );
-		$this->assertSame( '_papi_tab', $tab->id );
-		$this->assertSame( [], $tab->properties );
-	}
-}
