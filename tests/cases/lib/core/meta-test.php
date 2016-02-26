ceaaf13415cf068a8e21cc5f30c74456d896271b
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-02-26 07:49:14 +0100

    Add tests for papi_get_meta_id

--- tests/cases/lib/core/meta-test.php
+++ tests/cases/lib/core/meta-test.php
@@ -12,6 +12,15 @@ class Papi_Lib_Core_Meta_Test extends WP_UnitTestCase {
 		unset( $this->post_id );
 	}
 
+	public function test_papi_get_meta_id() {
+		$this->assertSame( 'post_id', papi_get_meta_id() );
+		$this->assertSame( 'post_id', papi_get_meta_id( 'post' ) );
+		$this->assertSame( 'post_id', papi_get_meta_id( 'page' ) );
+		$this->assertSame( 'term_id', papi_get_meta_id( 'term' ) );
+		$this->assertSame( 'term_id', papi_get_meta_id( 'taxonomy' ) );
+		$this->assertSame( 'post_id', papi_get_meta_id( 'hello' ) );
+	}
+
 	public function test_papi_get_meta_store() {
 		$store = papi_get_meta_store( $this->post_id );
 		$this->assertTrue( is_object( $store ) );
