2a130a3d8b0819aca337994bb6e6f08b805cecc0
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-04-12 16:06:29 +0200

    Fix object cache issue with existing post object when using overwrite on properties

--- src/pages/class-papi-post-page.php
+++ src/pages/class-papi-post-page.php
@@ -110,9 +110,13 @@ class Papi_Post_Page extends Papi_Core_Page {
 	 */
 	protected function prepare_convert_value( Papi_Core_Property $property, $value ) {
 		if ( $property->overwrite ) {
-			$slug  = $property->get_slug( true );
-			$post  = get_post( $this->id );
-			$value = $post->$slug;
+			// Clear post cache to solve issue with cached post objects
+			// when selecting post field.
+			clean_post_cache( $this->id );
+
+			$slug    = $property->get_slug( true );
+			$context = is_admin() ? 'edit' : 'display';
+			$value   = get_post_field( $slug, $this->id, $context );
 		}
 
 		return $value;
