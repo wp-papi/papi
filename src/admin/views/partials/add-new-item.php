d71aa81259ed669a098648bd4609fc7370e9280b
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-05-23 21:24:29 +0200

    Rename add new page css classes from post type to page type

--- src/admin/views/partials/add-new-item.php
+++ src/admin/views/partials/add-new-item.php
@@ -1,16 +1,16 @@
 <a href="<?php echo $vars['url']; ?>" class="papi-box-item">
 	<?php if ( ! empty( $vars['thumbnail'] ) ): ?>
-	<div class="papi-post-type">
-		<div class="papi-post-type-screenshot" style="background-image:url(<?php echo $vars['thumbnail']; ?>)">
+	<div class="papi-page-type">
+		<div class="papi-page-type-screenshot" style="background-image:url(<?php echo $vars['thumbnail']; ?>)">
 		</div>
 	</div>
 	<?php endif; ?>
-	<div class="papi-post-type-info">
+	<div class="papi-page-type-info">
 		<h3><?php echo $vars['title']; ?></h3>
 
 		<p><?php echo $vars['description']; ?></p>
 	</div>
-	<div class="papi-post-type-actions">
+	<div class="papi-page-type-actions">
 		<span class="button button-primary"><?php _e( 'Select', 'papi' ); ?></span>
 	</div>
 </a>
