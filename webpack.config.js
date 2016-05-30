d26f6d14e9bb9f43aaf154099f931f921e7ee68b
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-05-30 08:48:26 +0200

    Update outdated npm packages

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,10 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader'
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
       }
     ]
   },
