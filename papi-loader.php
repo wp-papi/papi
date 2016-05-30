961a230a2496bec9c333dd22db664ef9fdc92415
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-05-30 07:26:43 +0200

    Bump version

--- papi-loader.php
+++ papi-loader.php
@@ -5,7 +5,7 @@
  * Description: Page Type API with custom fields
  * Author: Fredrik Forsmo
  * Author URI: https://frozzare.com
- * Version: 3.1.0-beta2
+ * Version: 3.1.0-rc1
  * Plugin URI: https://wp-papi.github.io
  * Textdomain: papi
  * Domain Path: /languages/
