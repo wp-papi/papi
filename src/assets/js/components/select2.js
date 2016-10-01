cef689abff099f3af7265aad92757749fdcc4b76
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-10-01 12:50:06 +0200

    Add new js bundle

--- src/assets/js/components/select2.js
+++ src/assets/js/components/select2.js
@@ -8,8 +8,15 @@ const options = {
 
     const url      = $(result.element).data('edit-url');
     const editText = papiL10n.edit;
+    const $html = $('<div><span>' + result.text + '</span> <a href="#" class="papi-iframe-link-open" data-url="' + url + '&papi-iframe-mode=true">' + editText + '</a></div>');
 
-    return $('<span>' + result.text + '</span> <a href="#" class="papi-iframe-link-open" data-url="' + url + '&papi-iframe-mode=true">' + editText + '</a>');
+    $html.find('a').on('mousedown', function(e) {
+      e.stopPropagation();
+    }).on('click', function(e) {
+      e.preventDefault();
+    });
+
+    return $html;
   }
 };
 
