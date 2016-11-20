245212d17a5d9f0e215bd6c483f50e1527813320
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-11-20 17:53:28 +0100

    Fix rest api test

--- tests/cases/rest-api/class-papi-rest-api-post-test.php
+++ tests/cases/rest-api/class-papi-rest-api-post-test.php
@@ -11,7 +11,7 @@ class Papi_REST_API_Post_Test extends WP_UnitTestCase {
 		add_filter( 'papi/settings/directories', function () {
 			return PAPI_FIXTURE_DIR . '/page-types';
 		} );
-        
+
         $this->class = new Papi_REST_API_Post;
     }
 
@@ -21,9 +21,9 @@ class Papi_REST_API_Post_Test extends WP_UnitTestCase {
         unset( $this->class );
     }
 
-	public function test_actions() {
-		$this->assertSame( 10, has_action( 'the_post', [$this->class, 'get_post'] ) );
-	}
+    public function test_actions() {
+    	$this->assertSame( 10, has_action( 'the_post', [$this->class, 'get_post'] ) );
+    }
 
     public function test_get_post() {
         $post_id = $this->factory->post->create();
@@ -31,7 +31,7 @@ class Papi_REST_API_Post_Test extends WP_UnitTestCase {
         $this->assertSame( $post, $this->class->get_post( $post ) );
 
         update_post_meta( $post_id, papi_get_page_type_key(), 'simple-page-type' );
-        
+
         $this->assertSame( $post, $this->class->get_post( $post ) );
         $this->assertSame( 10, has_filter( 'rest_prepare_' . $post->post_type, [$this->class, 'prepare_response'] ) );
 
@@ -40,16 +40,16 @@ class Papi_REST_API_Post_Test extends WP_UnitTestCase {
         global $wp_meta_keys;
 
         foreach ( $page_type->get_properties() as $property ) {
-            $this->assertArrayHasKey( $property->get_slug( true ), $wp_meta_keys[$post->post_type] );
+        	$this->assertTrue( isset( $wp_meta_keys[$post->post_type][$property->get_slug( true )] ) );
         }
     }
 
     public function test_prepare_response() {
         $this->assertEmpty( $this->class->prepare_response( [] ) );
-        
+
         $post_id = $this->factory->post->create();
         $post = get_post( $post_id );
-        
+
         update_post_meta( $post_id, papi_get_page_type_key(), 'simple-page-type' );
         $this->assertSame( $post, $this->class->get_post( $post ) );
 
@@ -59,7 +59,7 @@ class Papi_REST_API_Post_Test extends WP_UnitTestCase {
                 'name' => get_post_meta( $post_id, 'name', true )
             ]
         ];
-        
+
         $response = $this->class->prepare_response( $response );
         $this->assertSame( 'Fredrik', $response['meta']['name'] );
     }
