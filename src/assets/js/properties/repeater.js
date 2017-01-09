ecc9371f293a0f40bea58c72b8c4f58303a3b590
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2017-01-09 14:55:51 +0100

    Fix issue when removing row in flexible with repeater existing

--- src/assets/js/properties/repeater.js
+++ src/assets/js/properties/repeater.js
@@ -318,7 +318,7 @@ class Repeater {
   updateRowNumber($tbody) {
     let self = this;
 
-    $tbody.find('> tr').each((i, el) => {
+    $tbody.first().find('> tr').each((i, el) => {
       let $el = $(el);
       let $rpt = $el.closest('.papi-property-repeater-top');
       let position = 0;
