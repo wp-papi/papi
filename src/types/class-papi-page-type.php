3498a14948eafc02cb03598fa3f11cea8920847f
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2017-03-31 10:45:13 +0200

    Remove unused code

--- src/types/class-papi-page-type.php
+++ src/types/class-papi-page-type.php
@@ -28,13 +28,6 @@ class Papi_Page_Type extends Papi_Entry_Type {
 	public $description = '';
 
 	/**
-	 * Disable metabox ordering.
-	 *
-	 * @var bool
-	 */
-	public $disable_metabox_ordering = false;
-
-	/**
 	 * Fill labels.
 	 *
 	 * @var bool
@@ -350,9 +343,6 @@ class Papi_Page_Type extends Papi_Entry_Type {
 			add_filter( 'enter_title_here', function () {
 				return $this->labels['title_placeholder'];
 			} );
-		}
-
-		if ( $this->disable_metabox_ordering ) {
 		}
 	}
 
