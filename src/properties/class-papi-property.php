83a54690f83e8506d15509256abf60aae62caa01
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2017-06-08 09:54:26 +0200

    Support array of langs

--- src/properties/class-papi-property.php
+++ src/properties/class-papi-property.php
@@ -56,8 +56,12 @@ class Papi_Property extends Papi_Core_Property {
 			return;
 		}
 
+		// Support array of langs.
+		$lang = $this->get_option( 'lang' );
+		$lang = is_array( $lang ) ? $lang : [$lang];
+
 		// Only render if it's the right language if the definition exist.
-		if ( $this->get_option( 'lang' ) === papi_get_lang() ) {
+		if ( in_array( papi_get_lang(), $lang, true )  ) {
 			$this->display = true;
 		} else {
 			$this->display = $this->get_option( 'lang' ) === false && papi_is_empty( papi_get_lang() );
