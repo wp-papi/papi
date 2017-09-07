5b0031f1397c79b27d775839873fe6e277b495df
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2017-09-07 13:36:22 +0200

    Add default default value to post options

--- src/properties/class-papi-property-post.php
+++ src/properties/class-papi-property-post.php
@@ -277,9 +277,9 @@ class Papi_Property_Post extends Papi_Property {
 						>
 
 							<?php if ( ! is_null( $settings->placeholder ) ): ?>
-								<option value="<?php echo esc_attr( $this->get_option( 'default' ) ); ?>"><?php esc_html( $placeholder ); ?></option>
+								<option value="<?php echo esc_attr( $this->get_option( 'default', '' ) ); ?>"><?php esc_html( $placeholder ); ?></option>
 							<?php elseif ( $settings->allow_clear ): ?>
-								<option value="<?php echo esc_attr( $this->get_option( 'default' ) ); ?>">&nbsp;</option>
+								<option value="<?php echo esc_attr( $this->get_option( 'default', '' ) ); ?>">&nbsp;</option>
 							<?php endif; ?>
 
 							<?php foreach ( $posts as $label => $items ) : ?>
