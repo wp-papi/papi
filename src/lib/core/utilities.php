2b55b9442cdefcc41cd6c5c285d26f2987620035
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2019-03-10 20:40:44 +0100

    Move papify/unpapify to slug file

--- src/lib/core/utilities.php
+++ src/lib/core/utilities.php
@@ -731,37 +731,3 @@ function papi_underscorify( $str ) {
 
 	return str_replace( ' ', '_', str_replace( '-', '_', $str ) );
 }
-
-/**
- * Add `papi_` to the given string ad the start of the string.
- *
- * @param  string $str
- *
- * @return string
- */
-function papify( $str = '' ) {
-	if ( ! is_string( $str ) ) {
-		return '';
-	}
-
-	if ( ! preg_match( '/^\_\_papi|^\_papi|^papi\_/', $str ) ) {
-		return str_replace( 'papi__', 'papi_', 'papi_' . $str );
-	}
-
-	return $str;
-}
-
-/**
- * Remove `papi-` or `papi_` from the given string.
- *
- * @param  string $str
- *
- * @return string
- */
-function unpapify( $str ) {
-	if ( ! is_string( $str ) ) {
-		return '';
-	}
-
-	return str_replace( 'papi-', '', str_replace( 'papi_', '', $str ) );
-}
--- tests/cases/lib/core/utilities-test.php
+++ tests/cases/lib/core/utilities-test.php
@@ -461,26 +461,4 @@ class Papi_Lib_Core_Utilities_Test extends WP_UnitTestCase {
 		$this->assertEmpty( papi_underscorify( [] ) );
 		$this->assertEmpty( papi_underscorify( new stdClass ) );
 	}
-
-	public function test_papify() {
-		$this->assertSame( 'papi_hello_world', papify( 'hello_world' ) );
-		$this->assertSame( 'papi_hello_world', papify( 'papi_hello_world' ) );
-		$this->assertSame( 'papi_hello_world', papify( '_hello_world' ) );
-		$this->assertEmpty( papify( null ) );
-		$this->assertEmpty( papify( true ) );
-		$this->assertEmpty( papify( false ) );
-		$this->assertEmpty( papify( 1 ) );
-		$this->assertEmpty( papify( [] ) );
-		$this->assertEmpty( papify( new stdClass ) );
-	}
-
-	public function test_unpapify() {
-		$this->assertSame( 'hello-world', unpapify( 'papi-hello-world' ) );
-		$this->assertEmpty( unpapify( null ) );
-		$this->assertEmpty( unpapify( true ) );
-		$this->assertEmpty( unpapify( false ) );
-		$this->assertEmpty( unpapify( 1 ) );
-		$this->assertEmpty( unpapify( [] ) );
-		$this->assertEmpty( unpapify( new stdClass() ) );
-	}
 }
