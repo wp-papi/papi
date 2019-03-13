3f29ddc12c74002669cf02eee725d8f830df5c39
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2019-03-13 11:53:54 +0100

    Remove åäö from box id

--- src/core/class-papi-core-box.php
+++ src/core/class-papi-core-box.php
@@ -137,7 +137,7 @@ class Papi_Core_Box {
 		}
 
 		if ( empty( $this->id ) ) {
-			$this->id = strtolower( papi_f( papi_underscorify( papify( $this->title ) ) ) );
+			$this->id = papi_slugify( strtolower( papi_f( papi_underscorify( papify( $this->title ) ) ) ) );
 			$this->id = sanitize_text_field( $this->id );
 		}
 	}
