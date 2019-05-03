ad43c6c0583aa49c42ea9e5dd5e124b825125bab
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2019-05-03 11:08:41 +0200

    Add flexible block to gutenberg test page type

--- tests/data/page-types/gutenberg-page-type.php
+++ tests/data/page-types/gutenberg-page-type.php
@@ -23,5 +23,97 @@ class Gutenberg_Page_Type extends Papi_Page_Type {
 			'type'  => 'string',
 			'title' => 'Book name'
 		] ) );
+
+		$this->block( [
+			'title'      => 'Modules',
+			papi_property( [
+				'sidebar'  => false,
+				'slug'     => 'modules',
+				'title'    => 'Modules',
+				'type'     => 'flexible',
+				'settings' => [
+					'layout' => 'row',
+					'items'  => [
+						[
+							'title' => 'Image & text block',
+							'slug'  => 'image-text-block',
+							'items' => [
+								papi_property( [
+									'title' => 'Title',
+									'type'  => 'string',
+									'slug'  => 'title',
+								] ),
+								papi_property( [
+									'title' => 'Text',
+									'type'  => 'text',
+									'slug'  => 'text',
+								] ),
+								papi_property( [
+									'title' => 'Image',
+									'type'  => 'image',
+									'slug'  => 'image',
+								] ),
+								papi_property( [
+									'title' => 'Link',
+									'type'  => 'link',
+									'slug'  => 'link',
+								] ),
+								papi_property( [
+									'title' => 'Button text',
+									'type'  => 'string',
+									'slug'  => 'button_text',
+								] ),
+							],
+						],
+						[
+							'title' => 'Files',
+							'slug'  => 'files',
+							'items' => [
+								papi_property( [
+									'title'    => 'Files',
+									'slug'     => 'files',
+									'type'     => 'repeater',
+									'settings' => [
+										'add_new_label' => 'Add new file',
+										'layout'        => 'table',
+										'items'         => [
+											papi_property( [
+												'title' => 'File',
+												'type'  => 'file',
+												'slug'  => 'source',
+											] ),
+
+											papi_property( [
+												'title' => 'Title',
+												'type'  => 'string',
+												'slug'  => 'title',
+											] ),
+										],
+									],
+								] ),
+
+								papi_property( [
+									'title' => 'Image',
+									'type'  => 'image',
+									'slug'  => 'image',
+								] ),
+
+								papi_property( [
+									'title' => 'Title',
+									'type'  => 'string',
+									'slug'  => 'title',
+								] ),
+
+								papi_property( [
+									'title' => 'Description',
+									'type'  => 'string',
+									'slug'  => 'description',
+								] ),
+							],
+						]
+					],
+				],
+			] ),
+		] );
 	}
 }
